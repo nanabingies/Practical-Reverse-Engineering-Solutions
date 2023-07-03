ac84bd598f41e9d74d5450c233b7b102d18a9d2b
Emmanuel Sasu <nanabingies1@gmail.com>
2023-07-03 12:41:16 +0000

    Update Question 1.cpp

--- Chapter 03/Asynchronous Procedure Calls (APC)/Question 1.cpp
+++ Chapter 03/Asynchronous Procedure Calls (APC)/Question 1.cpp
@@ -1,2 +1,24 @@
 #include "Question 1.h"
 
+EXTERN_C
+auto DriverUnload(_In_ PDRIVER_OBJECT DriverObject) -> void {
+	if (DriverObject->DeviceObject != nullptr)
+		IoDeleteDevice(DriverObject->DeviceObject);
+
+	return;
+}
+
+EXTERN_C
+auto DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING) -> NTSTATUS {
+
+	DriverObject->DriverUnload = DriverUnload;
+
+	auto ns = KernelModeApc();
+	if (!NT_SUCCESS(ns))	return STATUS_FAILED_DRIVER_ENTRY;
+
+	auto pid = reinterpret_cast<HANDLE>(3908);
+	ns = UserModeApc(pid);
+	if (!NT_SUCCESS(ns))	return STATUS_FAILED_DRIVER_ENTRY;
+
+	return STATUS_SUCCESS;
+}
