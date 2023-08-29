02c9d401488adcb41e95d0108c796a1e5c2b2a28
Nana Bingies <nanabingies1@gmail.com>
2023-08-29 23:43:23 +0000

    Update main.c

---
+++ Chapter 03/Sample D/main.c
@@ -0,0 +1,27 @@
+#include "IrpRoutines.h"
+#include "Process.h"
+
+NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	UNICODE_STRING drvName, dosName;
+	NTSTATUS status;
+	PDEVICE_OBJECT DeviceObject;
+
+	RtlInitUnicodeString(&drvName, L"\\Device\\ProcPanama");
+	status = IoCreateDevice(DriverObject, 0x1C, &drvName, FILE_DEVICE_UNKNOWN, NULL, FALSE, (PDEVICE_OBJECT*)&DeviceObject);
+	if (NT_SUCCES(status)) {
+		RtlInitUnicodeString(&dosName, L"\\DosDevices\\ProcPanama");
+
+		status = IoCreateSymbolicLink(&dosName, &drvName);
+		if (!NT_SUCCESS(status))
+			IoDeleteDevice(DeviceObject);
+	}
+
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = &IrpCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = &IrpCreateClose;
+
+	PsSetCreateProcessNotifyRoutine(PcreateProcessNotifyRoutine, FALSE);
+
+	return STATUS_SUCCESS;
+}
